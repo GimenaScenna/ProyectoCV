fb690e6dfd60deaa853fd28f6f9666fe3f851d2e
GimenaScenna <gimescenna@gmail.com>
2023-03-08 11:06:17 -0300

    Modificaciones para visualizacion

---
+++ functions.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById("boton1").addEventListener ("click",function () {
+    console.log("Capturamos el evento Click")
+    document.getElementById("perfil").classList.toggle("abrir")
+    })
+
+document.getElementById("boton2").addEventListener ("click",function () {
+    console.log("Capturamos el evento Click")
+    document.getElementById("FormacionAcademica").classList.toggle("abrir")
+    })
+
+
+
+    
