92af0a03bcbecd77bb4cc6f242431ef7e8a21d5a
GimenaScenna <gimescenna@gmail.com>
2023-03-08 15:21:18 -0300

    Modificaciones en jv y css

--- functions.js
+++ functions.js
@@ -1,14 +1,81 @@
+$(document).ready(function(){
+    $('.nav-item').on('click', function(e){
+        e.preventDefault();
+        var target = $(this).attr('href');
+
+        $('.nav-item.active').removeClass('active')
+        $(this).addClass('active')
 
 
-document.getElementById("boton1").addEventListener ("click",function () {
-    console.log("Capturamos el evento Click")
-    document.getElementById("perfil").classList.toggle("abrir")
+        $('.tab-item.active').fadeOut(100, function(){
+            $(this).removeClass('active')
+
+            $(target).fadeIn(400, function(){
+                $(this).addClass('active')
+            })
+        })
+
+        console.log('target', target)
     })
 
-document.getElementById("boton2").addEventListener ("click",function () {
-    console.log("Capturamos el evento Click")
-    document.getElementById("FormacionAcademica").classList.toggle("abrir")
-    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*document.getElementById("boton1").addEventListener ("click",function () {
+//     console.log("Capturamos el evento Click")
+//     document.getElementById("perfil").classList.toggle("abrir")
+//     })*/
+//  /*Para dar click en un elemento desplegable*/
+// function toggleClass(elem,className) {
+//     if (elem.className.indexOf (className) !== -1) {
+//         elem.className = elem.className.replace(className,"");
+//     }
+//     else{
+//         elem.className = elem.className.replace(/Vs+/g,"") + "" + className;
+//     }
+//     return elem;
+// }
+
+// /*Para seleccionar una de las opciones*/
+
+// function selectOption(e) {
+//     toggleClass(e.target.parentNote , "hide");
+//     const id = e.target.id;
+//     const newValue =
+// }
 
 
 
